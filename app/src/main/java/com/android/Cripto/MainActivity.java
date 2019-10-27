79541266f144d319d46e87f7584c7300023122de
AndresLQ1987 <andreslq1987.alq@gmail.com>
2019-10-27 03:14:26 -0700

    Añadido RSA para encriptar

--- app/src/main/java/com/android/Cripto/MainActivity.java
+++ app/src/main/java/com/android/Cripto/MainActivity.java
@@ -3,19 +3,27 @@ package com.android.Cripto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,10 +40,12 @@ public class MainActivity extends AppCompatActivity {
                 String user = text.getText().toString();
                 EditText text1 = findViewById(R.id.pt_password);
                 String pwd = text1.getText().toString();
-                boolean good = creaFile(user,pwd);
+                String cifredPwd = CifrarPwd(pwd);
+                boolean good = creaFile(user,pwd,cifredPwd);
                 TextView resul = findViewById(R.id.resultado);
                 if (good) {
-                    resul.setText("Guardado OK");
+                    File dir = getFilesDir();
+                    resul.setText(dir.toString());
                 } else {
                     resul.setText("No Guardado");
                 }
@@ -43,15 +53,41 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public boolean creaFile(String txt1, String txt2) {
-        String contentFile = creaContentFile(txt1,txt2);
+    private String CifrarPwd(String pwd) {
+        String cifred = "";
+        try {
+            RSA rsa = new RSA();
+            rsa.setContext(getBaseContext());
+            rsa.genKeyPair(1024);
+            rsa.saveToDiskPrivateKey("rsa.pi");
+            rsa.saveToDiskPublicKey("rsa.pb");
+            cifred = rsa.Encrypt(pwd);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+        return cifred;
+    }
+
+    public boolean creaFile(String txt1, String txt2, String txt3) {
+        String contentFile = creaContentFile(txt1,txt2,txt3);
+        FileOutputStream archivo;
         try
         {
-            OutputStreamWriter fout= new OutputStreamWriter(
-                    openFileOutput("users.xml", Context.MODE_PRIVATE));
-
-            fout.write(contentFile);
-            fout.close();
+            archivo = openFileOutput("user.txt",MODE_PRIVATE);
+            archivo.write(contentFile.getBytes());
+            archivo.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return false;
@@ -62,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public String creaContentFile(String user, String pwd) {
+    public String creaContentFile(String user, String pwd, String cifredPwd) {
         String xmlContent = "";
         xmlContent = xmlContent + "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
         xmlContent = xmlContent + "<content_file>\n";
@@ -70,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
         xmlContent = xmlContent + "        <time>" + dateActual() + "</time>\n";
         xmlContent = xmlContent + "        <usertext>" + user + "</usertext>\n";
         xmlContent = xmlContent + "        <pwdtext>" + pwd + "</pwdtext>\n";
-        //  xmlContent = xmlContent + "        <cifredpwd>" + cifredPwd + "</cidredpwd>\n";
+        xmlContent = xmlContent + "        <cifredpwd>" + cifredPwd + "</cidredpwd>\n";
         xmlContent = xmlContent + "    </data>\n";
         xmlContent = xmlContent + "</content_file>";
         return xmlContent;
@@ -79,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
     public String dateActual() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
         Date date = new Date();
-        String fecha = dateFormat.format(date);
-        return fecha;
+        return dateFormat.format(date);
     }
 }
